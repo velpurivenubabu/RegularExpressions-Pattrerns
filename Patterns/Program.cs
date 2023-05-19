9f8752e8fdf6856b2065bfbf25665019cbc7ea08
venubabu <velpurivenubabu123@gmail.com>
2023-05-19 21:46:39 +0530

    [Venubabu] Done With Welcome Message

---
+++ Patterns/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Patterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! Welcome to pattern programming");
+        }
+    }
+}
